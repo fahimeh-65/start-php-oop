fa08b5dd88510e815eee0d016a1beb1dbcbfee8d
fahimeh-65 <78345194+fahimeh-65@users.noreply.github.com>
2021-02-03 19:44:20 +0330

    create new file to

---
+++ oop1.php
@@ -0,0 +1,15 @@
+<?php
+
+class Rook 
+{
+
+     public $isonboard = true ;
+     public $color ;
+     public $square ;
+
+}
+
+$rook1 = new Rook () ;
+$rook2 = new Rook () ;
+$rook3 = new Rook () ;
+$rook4 = new Rook () ;
