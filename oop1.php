7ef979a8b7adf16e96f98fa8e035c928085fb0ff
Fahimeh Roohani <fahimehmmm@yahoo.com>
2021-02-03 20:39:32 +0330

    Create a function to construct the object and initialize it...

--- oop1.php
+++ oop1.php
@@ -9,10 +9,26 @@ class Rook
 
 }
 
-$rook1 = new Rook () ;
+//$rook1 = new Rook () ;
 // $rook2 = new Rook () ;
 // $rook3 = new Rook () ;
 // $rook4 = new Rook () ;
 
-$rook1 -> color = 'white' ;
-$rook1 -> square = 'h1' ;
+// $rook1 -> color = 'white' ;
+// $rook1 -> square = 'h1' ;
+
+function rookMaker($color,$square){
+     
+     $rook = new Rook() ; 
+     $rook -> color = $color ;
+     $rook -> square = $square ;
+
+     return $rook;
+}
+
+$rook1 = rookMaker('white','h1') ;
+$rook2 = rookMaker('white', 'h8') ;
+$rook3 = rookMaker('black', 'a1') ;
+$rook4 = rookMaker('black', 'a8') ;
+
+echo $rook3 -> color ;
