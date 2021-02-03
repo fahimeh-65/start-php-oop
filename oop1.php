566dd74e5fb4e32997273f1a31875022eac7187e
Fahimeh Roohani <fahimehmmm@yahoo.com>
2021-02-03 20:25:04 +0330

    initialize $rook1

--- oop1.php
+++ oop1.php
@@ -10,6 +10,9 @@ class Rook
 }
 
 $rook1 = new Rook () ;
-$rook2 = new Rook () ;
-$rook3 = new Rook () ;
-$rook4 = new Rook () ;
+// $rook2 = new Rook () ;
+// $rook3 = new Rook () ;
+// $rook4 = new Rook () ;
+
+$rook1 -> color = 'white' ;
+$rook1 -> square = 'h1' ;
