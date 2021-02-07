27ba2f89b61743437909447692b9af117b2a51c3
Fahimeh Roohani <fahimehmmm@yahoo.com>
2021-02-07 15:47:21 +0330

    Objects can change their properties

--- oop2.php
+++ oop2.php
@@ -2,23 +2,31 @@
 
 class A
 {
-    public $foo = 'iman' ;
+    //public static $foo = 'iman' ;
 
 }
 
 class B
 {
+    public $foo = 12 ;
     public function hello(){
-        echo 'hello' ;
+
+        // self :: $foo  ;
+        //echo 'hello' ;
+        $this -> foo += 2 ;
     }
 }
 
-$a = new A() ;
-$a2 = clone $a ;
+// $a = new A() ;
+// $a2 = clone $a ;
 
-$a ->foo = 'ali' ;
-$a2 -> foo = 'ahmad' ;
+// $a ->foo = 'ali' ;
+// $a2 -> foo = 'ahmad' ;
 
-echo $a -> foo;
-echo $a2 -> foo;
-    
+// echo $a -> foo;
+// echo $a2 -> foo;
+$b = new B() ;
+$b -> hello() ;
+echo $b -> foo ;
+
+
